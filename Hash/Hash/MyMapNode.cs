f9aa8237aa324c253ad7412e5b916484dfb2fcce
Rajangane <pratiksharajangane7@gmail.com>
2021-12-11 20:22:16 +0530

    [Pratiksha] ADD : Finding Frequency of words in a sentence

---
+++ Hash/Hash/MyMapNode.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    public class MyMapNode<K, V>
+    {
+        /// <summary>
+        /// variables
+        /// </summary>
+        private readonly int size; //size of hash table
+        private readonly LinkedList<KeyValue<K, V>>[] items; //Passing the Keyvalue object to the Linkedlist
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="size"></param>
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size; //identifying hash code of key
+            return Math.Abs(position); //absolute value to identify integer value of key
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position) // position will be the index
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        /// <summary>
+        /// adding two parameters and  pushing data into hash table
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(K key, V value) 
+        {
+            int position = GetArrayPosition(key);  
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key); // identifying at what index, key is present
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+    }
+
+    /// <summary>
+    /// class key value
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
--- Hash/Hash/Program.cs
+++ Hash/Hash/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("************** Welcome To Hashing ****************");
+//Console.WriteLine("************** Welcome To Hashing ****************");
+using Hash;
+Console.Write("Select Number:\n1)ToBeOrNotToBe");
+int option = Convert.ToInt32(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        hash.Add("0", "To");
+        hash.Add("1", "Be");
+        hash.Add("2", "Or");
+        hash.Add("3", "Not");
+        hash.Add("4", "To");
+        hash.Add("5", "Be");
+        string hash5 = hash.Get("5");
+        Console.WriteLine("5th index value : {0} ", hash5);
+        string hash2 = hash.Get("2");
+        Console.WriteLine("2nd index value : {0} ", hash2);
+         break;
+
+    default:
+        Console.WriteLine("invalid option");
+        break;
+}
